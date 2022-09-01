41e09b5cbfd4ec37946e059d47f42cd8ce2e9918
Emma Forman Ling <emma@convex.dev>
2022-08-31 21:33:43 -0700

    map moves to center on input

--- swim-spots/pages/index.tsx
+++ swim-spots/pages/index.tsx
@@ -1,12 +1,21 @@
 import { Status, Wrapper } from "@googlemaps/react-wrapper";
 import type { NextPage } from "next";
-import { useEffect, useRef, useState } from "react";
+import {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const GOOGLE_API_KEY: string = process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "";
-// TODO get current location for center
-const center = { lat: -34.397, lng: 150.644 };
-const zoom = 4;
+const ZOOM: number = 4;
+const CENTER: google.maps.LatLngLiteral = {
+  lat: 0,
+  lng: 0,
+};
 
 const ErrorComponent = <div>Something went wrong.</div>;
 
@@ -17,40 +26,88 @@ const render = (status: Status) => {
     case Status.FAILURE:
       return ErrorComponent;
     case Status.SUCCESS:
-      return <Map center={center} zoom={zoom} />;
+      return <h3>success</h3>;
   }
 };
 
 function Map({
-  center,
-  zoom,
+  map,
+  setMap,
 }: {
-  center: google.maps.LatLngLiteral;
-  zoom: number;
+  map: google.maps.Map | undefined;
+  setMap: Dispatch<SetStateAction<google.maps.Map | undefined>>;
 }) {
   const ref = useRef<HTMLDivElement>(null);
-  const [map, setMap] = useState<google.maps.Map>();
 
   useEffect(() => {
     if (ref.current && !map) {
       setMap(
         new window.google.maps.Map(ref.current, {
-          center,
-          zoom,
+          center: CENTER,
+          zoom: ZOOM,
         })
       );
     }
-  }, [center, zoom, map]);
+  }, [map, setMap, ref]);
 
   return <div ref={ref} id="map" />;
 }
 
+interface AddressInputProps {
+  map: google.maps.Map | undefined;
+}
+
+function AddressInput({ map }: AddressInputProps) {
+  const [address, setAddress] = useState("");
+  const geocoder = new google.maps.Geocoder();
+  const codeAddress = (event: FormEvent) => {
+    event.preventDefault();
+    geocoder.geocode({ address: address }, function (results, status) {
+      if (status == "OK") {
+        if (results) {
+          const firstResult = results[0].geometry.location;
+          const center = {
+            lat: firstResult.lat(),
+            lng: firstResult.lng(),
+          };
+          if (map) {
+            map.setCenter(center);
+          }
+        } else {
+          throw Error("Results from geocoding request was null");
+        }
+      } else {
+        console.error(
+          `Request to geocoder failed with status ${status} and results ${results}`
+        );
+      }
+    });
+  };
+  return (
+    <form onSubmit={codeAddress} className="d-flex justify-content-center">
+      <input
+        value={address}
+        onChange={(event) => setAddress(event.target.value)}
+        className="form-control w-50"
+        placeholder="Location..."
+      />
+      <input
+        type="submit"
+        value="Search"
+        className="ms-2 btn btn-primary"
+        disabled={!address}
+      />
+    </form>
+  );
+}
+
 const MyApp = () => {
+  const [map, setMap] = useState<google.maps.Map>();
   return (
     <Wrapper apiKey={GOOGLE_API_KEY} render={render}>
       <h1>Swim Spots</h1>
-
-      <Map center={center} zoom={zoom} />
+      {!!map && <AddressInput map={map} />}
+      <Map map={map} setMap={setMap} />
     </Wrapper>
   );
 };
