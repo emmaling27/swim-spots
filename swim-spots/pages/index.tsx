098056e5f31633acc0d45d692a813067816362f1
Emma Forman Ling <emma@convex.dev>
2022-09-08 10:17:19 +0100

    use material UI

--- swim-spots/pages/index.tsx
+++ swim-spots/pages/index.tsx
@@ -9,9 +9,13 @@ import {
   useState,
 } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 const GOOGLE_API_KEY: string = process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "";
 const ZOOM: number = 4;
+
+// TODO: initialize to the user's current location
 const CENTER: google.maps.LatLngLiteral = {
   lat: 0,
   lng: 0,
@@ -84,20 +88,17 @@ function AddressInput({ map }: AddressInputProps) {
     });
   };
   return (
-    <form onSubmit={codeAddress} className="d-flex justify-content-center">
-      <input
+    <div>
+      <TextField
+        id="outlined-name"
+        label="Location"
         value={address}
         onChange={(event) => setAddress(event.target.value)}
-        className="form-control w-50"
-        placeholder="Location..."
       />
-      <input
-        type="submit"
-        value="Search"
-        className="ms-2 btn btn-primary"
-        disabled={!address}
-      />
-    </form>
+      <Button onClick={codeAddress} value="Search" disabled={!address}>
+        Search
+      </Button>
+    </div>
   );
 }
 
